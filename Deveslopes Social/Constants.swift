521b00aa4db0d6d73b4155282878267cca6ef24d
chth12 <ctoddh12@gmail.com>
2016-12-02 12:37:09 -0500

    Sign in ui cutom classes

---
+++ Deveslopes Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Deveslopes Social
+//
+//  Created by Christopher Heins on 12/2/16.
+//  Copyright © 2016 Christopher Todd Heins. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 12.0 / 255.0
