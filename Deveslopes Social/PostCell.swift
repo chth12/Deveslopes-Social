038a46f6bbb9c0fe7bba2fe84a01dbe763cf530f
chth12 <ctoddh12@gmail.com>
2016-12-06 14:43:53 -0500

    feed UI complete

---
+++ Deveslopes Social/PostCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostCell.swift
+//  Deveslopes Social
+//
+//  Created by Christopher Heins on 12/6/16.
+//  Copyright © 2016 Christopher Todd Heins. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likes: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
